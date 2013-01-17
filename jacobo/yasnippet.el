7bbfdeb396ae9fd270023e0a2102d94cb9899d1f
Daniel Mijares <daniel.j.mijares@gmail.com>
2013-01-17 14:26:57 -0500

    modifying the yasnippets to include othere directories in java-mode-plus and make it easier to add other directories in the future

--- jacobo/yasnippet.el
+++ jacobo/yasnippet.el
@@ -1,9 +1,11 @@
 (require 'yasnippet)
 
 (yas-global-mode 1)
-(setq yas-snippet-dirs
-      '("~/.emacs.d/snippets"            ;; personal snippets
-;;        "~/.emacs.d/plugins/yasnippet/snippets"    ;; the default collection
-        ))
+
+(setq yas/root-directory '("~/.emacs.d/snippets"
+                           "~/.emacs.d/plugins/java-mode-plus/snippets"))
+
+;; Map `yas/load-directory' to every element
+(mapc 'yas/load-directory yas/root-directory)
 
 (provide 'jacobo/yasnippet)
