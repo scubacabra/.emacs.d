31e1b0a4cf381be37a5cdc90baede1501d403e5c
Daniel Mijares <daniel.j.mijares@gmail.com>
2013-01-17 23:44:10 -0500

    adding gradle-mode minor mode base functionality and including in the init.el

--- jacobo/groovy.el
+++ jacobo/groovy.el
@@ -4,6 +4,8 @@
 (add-to-list 'auto-mode-alist '("\\.groovy$" . groovy-mode))
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
 (add-to-list 'auto-mode-alist '("\\.gradle$" . groovy-mode))
+(require 'gradle-mode)
+
 ;;(require 'groovy-eval)
 (autoload 'groovy-eval "groovy-eval" "Groovy Evaluation" t)
 (add-hook 'groovy-mode-hook 'groovy-eval)
