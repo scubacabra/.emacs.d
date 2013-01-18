6d78083ee1b787f2dd55685717d9f402648df0f2
Daniel Mijares <daniel.j.mijares@gmail.com>
2013-01-17 23:23:56 -0500

    adding support for groovy-mode-plus and adding support for another yasnippet in the groovy-mode-plus folder

--- jacobo/groovy.el
+++ jacobo/groovy.el
@@ -1,4 +1,6 @@
 (require 'groovy-mode)
+(require 'groovy-mode-plus)
+
 (add-to-list 'auto-mode-alist '("\\.groovy$" . groovy-mode))
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
 (add-to-list 'auto-mode-alist '("\\.gradle$" . groovy-mode))
