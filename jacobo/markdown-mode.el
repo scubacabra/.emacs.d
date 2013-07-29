5c2587809dd622c0cfd3bba543cea30b5d50e7ca
Daniel Mijares <daniel.j.mijares@gmail.com>
2013-07-29 14:11:37 -0400

    markdown is now a submodule, deleting the old file, was very behind

--- jacobo/markdown-mode.el
+++ jacobo/markdown-mode.el
@@ -1,5 +1,5 @@
 (require 'markdown-mode)
 
 (setq auto-mode-alist  (cons '("\\.md" . markdown-mode) auto-mode-alist))
-
+;;(add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
 (provide'jacobo/markdown-mode)
