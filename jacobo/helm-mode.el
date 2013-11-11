ecfcf454e97b445c97c843614ece8d48564bcb76
Daniel Mijares <daniel.j.mijares@gmail.com>
2013-11-11 17:01:17 -0500

    helm mode ls-git and find files keybindgs

--- jacobo/helm-mode.el
+++ jacobo/helm-mode.el
@@ -1,6 +1,7 @@
 (global-set-key (kbd "C-c h") 'helm-mini)
 (global-set-key (kbd "C-c sk") 'helm-show-kill-ring)
-(global-set-key (kbd "C-c gf") 'helm-git-find-files)
+(global-set-key (kbd "C-c gf") 'helm-ls-git-ls)
+(global-set-key (kbd "C-x f") 'helm-find-files)
 (helm-mode 1)
 (setq enable-recursive-minibuffers t)
 
