5a0f91a351b2b64e69d07156980548daf2c22594
Daniel Mijares <Daniel.J.Mijares@raytheon.com>
2013-11-20 09:46:49 -0500

    re-adding the groovy-mode-plus yasnippets and java-mode-plus yassnippets to the yasnippet load path.  Just a little re-work -- a little behind on the curr yasnippet setup , had old manual

--- jacobo/yasnippet.el
+++ jacobo/yasnippet.el
@@ -1,9 +1,7 @@
+(setq yas-snippet-dirs
+      '("~/.emacs.d/snippets"
+	"~/.emacs.d/plugins/java-mode-plus/snippets"
+	"~/.emacs.d/plugins/groovy-mode-plus/snippets"))
+
 (yas-global-mode 1)
-
-(setq yas/root-directory "~/.emacs.d/snippets")
-;;                           "~/.emacs.d/plugins/java-mode-plus/snippets"
-;;			   "~/.emacs.d/plugins/groovy-mode-plus/snippets")
-
-(yas-load-directory yas/root-directory)
-
 (provide 'jacobo/yasnippet)
