b945ac9f3f7a3b01527b79a988330dad370b45ca
Daniel Mijares <Daniel.J.Mijares@raytheon.com>
2013-11-20 10:51:37 -0500

    add java import key map

--- jacobo/javadoc.el
+++ jacobo/javadoc.el
@@ -1,4 +1,6 @@
 (require 'javadoc-lookup)
+
 (define-key global-map (kbd "C-c jd") 'javadoc-lookup)
 (javadoc-add-roots "/Users/djmijares/dev/worldwind/doc")
+(define-key global-map (kbd "C-c ji") 'add-java-import)
 (provide 'jacobo/javadoc)
