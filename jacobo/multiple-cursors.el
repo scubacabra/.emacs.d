c88dacfa4dca9bcb55e593b9a192e3e76b3b5984
Daniel Mijares <Daniel.J.Mijares@raytheon.com>
2013-12-13 16:14:41 -0500

    multiple cursors able to cycle through cursors now weee

--- jacobo/multiple-cursors.el
+++ jacobo/multiple-cursors.el
@@ -1,3 +1,5 @@
+(require 'mc-cycle-cursors)
+
 ;; Experimental multiple-cursors
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
@@ -9,5 +11,7 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-M-m") 'mc/mark-more-like-this-extended)
 (global-set-key (kbd "M-A") 'mc/mark-all-in-region)
+(global-set-key (kbd "C-S-s") 'mc/skip-to-next-like-this)
+(global-set-key (kbd "C-S-b") 'mc/skip-to-previous-like-this)
 
 (provide 'jacobo/multiple-cursors)
