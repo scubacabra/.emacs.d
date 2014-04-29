7ab008ab2b6e2e0c365aafeb0276927e4ab3bf34
Daniel Mijares <daniel.j.mijares@gmail.com>
2014-04-29 19:41:08 -0400

    new bindings for smex to override current mex.  Don't know why I wasn't using this before

--- jacobo/smex.el
+++ jacobo/smex.el
@@ -1,5 +1,6 @@
 ;; Use C-x C-m to do M-x per Steve Yegge's advice
-(global-set-key (kbd "C-c M-s") 'smex)
-(global-set-key (kbd "C-c M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 (provide 'jacobo/smex)
