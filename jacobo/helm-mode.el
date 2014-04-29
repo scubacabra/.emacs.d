b45854041b22f14210036dd6e3f103f043eea5d6
Daniel Mijares <daniel.j.mijares@gmail.com>
2014-04-29 19:52:21 -0400

    new helm not performing auto completion -- fixing that

--- jacobo/helm-mode.el
+++ jacobo/helm-mode.el
@@ -3,6 +3,6 @@
 (global-set-key (kbd "C-c C-h g") 'helm-ls-git-ls)
 (global-set-key (kbd "C-x f") 'helm-find-files)
 (helm-mode 1)
-(setq enable-recursive-minibuffers t)
+(setq helm-ff-auto-update-initial-value t)
 
 (provide 'jacobo/helm-mode)
