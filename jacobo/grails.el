4bb1acdd12129f6fa6ecb3cd88804d0e7d201b5a
Daniel Mijares <daniel.j.mijares@gmail.com>
2014-12-07 17:10:35 -0500

    hook into grails mode to load grails yasnippets

--- jacobo/grails.el
+++ jacobo/grails.el
@@ -1,7 +1,11 @@
-
 (require 'grails-projectile-mode)
 
-(setq grails-projectile-keymap-prefix (kbd "C-c ;"))
-(grails-projectile-global-mode t)
+;; When entering grails-mode, consider also the snippets in the
+;; snippet table "grails-mode"
+(add-hook 'grails-projectile-mode-hook
+          #'(lambda ()
+              (yas-activate-extra-mode 'grails-mode)))
+
+(add-hook 'projectile-mode-hook 'grails-projectile-on)
 
 (provide 'jacobo/grails)
