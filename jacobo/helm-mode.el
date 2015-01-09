20b801afd9497224e4219306ce468124e8825c47
Daniel Mijares <daniel.j.mijares@gmail.com>
2015-01-08 21:37:23 -0500

    helm additions, mini is bound to c-x b and list-buffers to c-x B
    no real need for ace jump buffers with helm-mini
    
    autocomplete is deleted finally
    
    helm-descbinds installed
    
    no more smex

--- jacobo/helm-mode.el
+++ jacobo/helm-mode.el
@@ -6,10 +6,12 @@
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
 
-(global-set-key (kbd "C-c h m") 'helm-mini)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x B") 'switch-to-buffer)
 (global-set-key (kbd "C-c h g") 'helm-ls-git-ls)
 (global-set-key (kbd "C-x f") 'helm-find-files)
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
 
 (when (executable-find "curl")
   (setq helm-google-suggest-use-curl-p t))
@@ -20,7 +22,11 @@
       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-      helm-ff-file-name-history-use-recentf t)
+      helm-ff-file-name-history-use-recentf t
+
+      helm-semantic-fuzzy-match t
+
+      helm-imenu-fuzzy-match t)
 
 (require 'helm-swoop)
 (global-set-key (kbd "M-i") 'helm-swoop)
@@ -45,6 +51,10 @@
 
 ;; If nil, you can slightly boost invoke speed in exchange for text color
 (setq helm-swoop-speed-or-color nil)
+
+(require 'helm-descbinds)
+(helm-descbinds-mode)
+(global-set-key (kbd "C-c D") 'helm-descbinds)
 
 (helm-mode 1)
 
