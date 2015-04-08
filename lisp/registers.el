ba3a67d297e268c4693015700a91f42feb2b4299
Daniel Mijares <daniel.j.mijares@gmail.com>
2015-04-07 21:00:55 -0400

    new registers for keysnail

--- lisp/registers.el
+++ lisp/registers.el
@@ -8,6 +8,10 @@
 (set-register ?b '(file . "~/.bashrc"))
 (set-register ?f '(file . "~/.config/fish/config.fish"))
 (set-register ?o '(file . "~/.oh-my-fish/oh-my-fish.fish"))
+(set-register ?k '(file . "~/.keysnail.js"))
+(set-register
+ ?m '(file
+      . "/Users/djmijares/Library/Application Support/Karabiner/private.xml"))
 
 (provide 'registers)
 ;;; registers.el ends here
