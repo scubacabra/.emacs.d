580295be53e20c1fe7c90bbd17b9b7df46e9b71b
Daniel Mijares <daniel.j.mijares@gmail.com>
2015-06-16 08:50:23 -0400

    smart mode line tweaking

--- lisp/jacobo/smart-mode-line.el
+++ lisp/jacobo/smart-mode-line.el
@@ -1,14 +1,20 @@
+(require 'powerline)
 (require 'smart-mode-line)
+(require 'nyan-mode)
 
+(setq powerline-default-separator 'slant)
 (sml/setup)
 (sml/apply-theme 'powerline)
 
 (setq sml/shorten-modes t)
 (setq sml/shorten-directory t)
-(setq sml/name-width 40)
-(setq sml/mode-width 'full)
+(setq sml/name-width 15)
+(setq sml/mode-width 15)
+(setq sml/position-percentage-format "")
+(setq sml/use-projectile-p 'before-prefixes)
 
 (setq rm-blacklist '(" GitGutter" " Gradle" " Grails" " company" " yas"
-		     " Helm" " MRev" " Smrt"))
+		     " Helm" " MRev" " Smrt" " wb"))
 
+(setq nyan-bar-length 20)
 (provide 'jacobo/smart-mode-line)
