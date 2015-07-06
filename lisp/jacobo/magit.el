299536e0719f8360b1d17218d1eeded504950e4a
Daniel Mijares <daniel.j.mijares@gmail.com>
2015-07-06 10:08:59 -0400

    changes for magit 2.1.0 :)

--- lisp/jacobo/magit.el
+++ lisp/jacobo/magit.el
@@ -4,8 +4,7 @@
 (global-set-key (kbd "C-x m") 'magit-status) 
 (autoload 'magit-status "magit")
 
-; Subtler highlight
-(set-face-background 'magit-item-highlight "#121212")
+;; Subtler highlight
 (set-face-foreground 'diff-context "#666666")
 (set-face-foreground 'diff-added "#00cc33")
 (set-face-foreground 'diff-removed "#ff0000")
